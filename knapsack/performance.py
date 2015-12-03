8a7330757600e469c3c55ab1eb0c2482460568ec
MichalP <mpp@linuxmail.org>
2015-12-03 11:52:08 +0100

    tests improved

--- knapsack/performance.py
+++ knapsack/performance.py
@@ -6,6 +6,8 @@ from knapsack import readInstance, generateInstance
 from time import time
 import sys
 
+sys.setrecursionlimit(100000)
+
 if len(sys.argv) != 6:
     print "Usage: {} type prefix minsize step approx".format(sys.argv[0])
     sys.exit()
@@ -29,26 +31,33 @@ if t == 'time':
             begin = time()
             dynResult, _ = knapsack(n, c, w)
             dynTime = time() - begin
-            counter[j][i] = [dynResult, fptasResult, dynTime, fptasTime]
+            # counter[j][i] = [dynResult, fptasResult, dynTime, fptasTime]
             fptasSum += fptasTime
             dynSum += dynTime
         averages.append([n, dynSum/100.0, fptasSum/100.0, abs(dynSum-fptasSum)/float(dynSum)*100])
         j += 1
         fptasSum, dynSum = 0, 0
-
-    for n in averages:
-        print n
+    filename = "tests/res_{0}_{1}_{3}_t_{2}".format(prefix, str(step), str(approx), str(minsize))
+    with open(filename, 'w') as f:
+        for n in averages:
+            print n
+            f.write(str(n)[1:-1]+"\n")
 
 elif t == 'quality':
     res = []
     for n in range(minsize, 10*step + minsize, step):
-        filename = "tests/{0}_{2}_quality_s{1}".format(prefix, str(step), str(approx))
+        filename = "tests/{0}_{2}_quality_s{1}.csv".format(prefix, str(step), str(approx))
         generateInstance(filename, int(n), int(n)*randint(1,n))
         n, c, w = readInstance(filename)
         fptas, _ = FPTAS(n, c, w, approx)
         dyn, _ = knapsack(n, c, w)
         q = abs(dyn-fptas)/float(dyn)
         res.append([n, dyn, fptas, q, q * 100])
+    filename = "tests/res_{0}_{1}_{3}_q_{2}.csv".format(prefix, str(step), str(approx), str(minsize))
+    with open(filename, 'w') as f:
+        for n in res:
+            print n
+            f.write(str(n)[1:-1]+"\n")
 
 else:
     print "Analysis type not found"
